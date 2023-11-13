d4d6847c6ed3871fb8f95b5bc0961d4b17bce251
Nigel Metheringham <nigel.metheringham@gmail.com>
2023-11-13 10:58:36 +0000

    test: Convert unittest to pytest

---
+++ tests/django_settings.py
@@ -0,0 +1,33 @@
+import os
+from typing import Any
+
+
+BASE_DIR = os.path.dirname(__file__)
+SETTINGS_MODULE = "tests.django_settings"
+
+INSTALLED_APPS = ("bootstrap_pagination",)
+
+DATABASES: dict[str, Any] = {}
+MIDDLEWARE_CLASSES = ()
+
+ROOT_URLCONF = "tests.django_settings.urls"
+SECRET_KEY = "secretkey"
+SITE_ROOT = "."
+
+
+TEMPLATE_DEBUG = True
+TEMPLATE_DIRS = ()
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": TEMPLATE_DIRS,
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [],
+            "debug": TEMPLATE_DEBUG,
+        },
+    },
+]
+
+urls: list[Any] = []
--- tests/test_pager.py
+++ tests/test_pager.py
@@ -1,14 +1,10 @@
-try:
-    import unittest2 as unittest
-except ImportError:
-    import unittest
-
 import lxml.html
 
 try:
     from django.template.loader import get_template_from_string
 except ImportError:
     from django.template import Engine
+
     get_template_from_string = Engine.get_default().from_string
 
 from django.template import Context
@@ -16,19 +12,21 @@ import django.http
 from django.core.paginator import Paginator
 
 
-class PagerTestCase(unittest.TestCase):
+class TestPagerTestCase:
     def test_example(self):
-        template = get_template_from_string("""
+        template = get_template_from_string(
+            """
             {% load bootstrap_pagination %}
             {% bootstrap_pager page_obj %}
-        """)
+        """,
+        )
 
-        objects = ["obj%02x" % idx
-                   for idx in range(30)]
+        objects = ["obj%02x" % idx for idx in range(30)]
 
         paginator = Paginator(objects, 10)
 
-        c = Context({'page_obj': paginator.page(2),
-                     'request': django.http.HttpRequest()})
+        c = Context(
+            {"page_obj": paginator.page(2), "request": django.http.HttpRequest()},
+        )
         html = lxml.html.fragment_fromstring(template.render(c))
-        self.assertEqual(html.get('class').strip(), 'pager')
+        assert html.get("class").strip() == "pager"
